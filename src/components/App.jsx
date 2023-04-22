b27765bec0a78155d798693f219f1df75a82adce
SemenZvehintsev <kvadratniibomzh@gmail.com>
2023-04-23 00:15:09 +0300

    add layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,7 @@
+import { MainLayout } from "pages/MainLayout/MainLayout";
+
 export const App = () => {
     return (
-        <div
-            style={{
-                height: '100vh',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: 40,
-                color: '#010101',
-            }}
-        >
-            goosetrack-team-project-front
-        </div>
+        <MainLayout/>
     );
 };

ca6ef5d12ffdd65fae0546ed2b6b915e9c4529f1
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-04-22 09:22:21 +0300

    fix chip

--- src/core/kit/Chip.jsx
+++ src/core/kit/Chip.jsx
@@ -1,63 +1,34 @@
 import styled from "styled-components";
 import propTypes from "prop-types";
 
-export const Chip = ({ priority }) => {
-  if (priority === "low") return <LowContainer>Low</LowContainer>;
-  if (priority === "med") return <MedContainer>Medium</MedContainer>;
-  if (priority === "high") return <HighContainer>High</HighContainer>;
+const Container = styled.div(({ theme, priority }) => ({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: "4px 12px",
+  gap: "149px",
+  borderRadius: "4px",
+  backgroundColor:
+    priority === "Low"
+      ? theme.color.taskLowColor
+      : priority === "Medium"
+      ? theme.color.taskMedColor
+      : theme.color.taskHighColor,
+  color: "#f7f6f9",
+  fontFamily: "Inter",
+  fontWeight: "600",
+  fontSize: "10px",
+  lineHeight: "12px",
+  textAlign: "center",
+}));
+
+const Chip = ({ priority }) => {
+  return <Container priority={priority}>{priority}</Container>;
 };
 
 Chip.propTypes = {
-  priority: propTypes.string.isRequired,
+  priority: propTypes.oneOf(["Low", "Medium", "High"]).isRequired,
 };
 
-export const LowContainer = styled.div(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "4px 12px",
-  gap: "149px",
-  borderRadius: "4px",
-  backgroundColor: theme.color.taskLowColor,
-  color: "#f7f6f9",
-  fontFamily: "Inter",
-  fontWeight: "600px",
-  fontSize: "10px",
-  lineHeight: "12px",
-  textAlign: "center",
-}));
-
-export const MedContainer = styled.div(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "4px 12px",
-  gap: "149px",
-  borderRadius: "4px",
-  backgroundColor: theme.color.taskMedColor,
-  color: "#f7f6f9",
-  fontFamily: "Inter",
-  fontWeight: "600px",
-  fontSize: "10px",
-  lineHeight: "12px",
-  textAlign: "center",
-}));
-
-export const HighContainer = styled.div(({ theme }) => ({
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center",
-  alignItems: "center",
-  padding: "4px 12px",
-  gap: "149px",
-  borderRadius: "4px",
-  backgroundColor: theme.color.taskHighColor,
-  color: "#f7f6f9",
-  fontFamily: "Inter",
-  fontWeight: "600px",
-  fontSize: "10px",
-  lineHeight: "12px",
-  textAlign: "center",
-}));
+export default Chip;

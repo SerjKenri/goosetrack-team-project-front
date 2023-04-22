d6e5605b10ed6bfc50f98894c2915e47b7fc7ab6
Max Sidorenko <fivemoshr@gmail.com>
2023-04-22 12:10:52 +0300

    feat(core):icon button

--- src/core/theme/colors.jsx
+++ src/core/theme/colors.jsx
@@ -1,42 +1,43 @@
 const colorsLight = {
   /* text */
-  mainTextColor: '#111111',
-  secondaryTextColor: '#616161',
-  accentTextColor: '#3e85f3',
-  activeSelectionColor: '#e3f3ff',
-  btnTextColor: '#ffffff',
-  inactiveBtnTextColor: '#84828a',
-  calendarDateColor: '#343434',
-  calendarCellColor:'#FFFFFF',
-  accentColor: '#3e85f3',
+  mainTextColor: "#111111",
+  secondaryTextColor: "#616161",
+  accentTextColor: "#3e85f3",
+  activeSelectionColor: "#e3f3ff",
+  btnTextColor: "#ffffff",
+  inactiveBtnTextColor: "#84828a",
+  calendarDateColor: "#343434",
+  calendarCellColor: "#FFFFFF",
+  accentColor: "#3e85f3",
 
   /* backgroung */
-  mainBackgroundColor: '#ffffff',
-  authBackgroundColor: '#dcebf7',
-  outletBackgroundColor: '#f7f6f9',
-  sidebarBackgroundColor: '#ffffff',
-  accentBackgroundColor: '#3e85f3',
-  modalBorder:'#DCE3E5CC',
+  mainBackgroundColor: "#ffffff",
+  authBackgroundColor: "#dcebf7",
+  outletBackgroundColor: "#f7f6f9",
+  sidebarBackgroundColor: "#ffffff",
+  accentBackgroundColor: "#3e85f3",
+  modalBorder: "#DCE3E5CC",
 
   /* tasks */
-  taskHighColor: '#ea3d65',
-  taskMedColor: '#f3b249',
-  taskLowColor: '#72c2f8',
-  taskCardColor: '#dce3e5cc',
-  inputFieldColor: '#f6f6f6',
-  taskCancelColor: '#efefef',
-  inputFieldTextColor: '#616161',
+  taskHighColor: "#ea3d65",
+  taskMedColor: "#f3b249",
+  taskLowColor: "#72c2f8",
+  taskCardColor: "#dce3e5cc",
+  inputFieldColor: "#f6f6f6",
+  taskCancelColor: "#efefef",
+  inputFieldTextColor: "#616161",
+  iconColor: "#111111",
 
   /* calendar */
-  priorityHighColor: '#ffd2dd',
-  priorityMedColor: '#fcf0d4',
-  priorityLowColor: '#ceeefd',
+  priorityHighColor: "#ffd2dd",
+  priorityMedColor: "#fcf0d4",
+  priorityLowColor: "#ceeefd",
 
   /* other */
-  scrollbarColor: '#e7e5e5',
-  scrollbarBackgroundColor: '#f2f2f2',
-  frameBorderColor: '#e1e7e9',
-}
+  scrollbarColor: "#e7e5e5",
+  scrollbarBackgroundColor: "#f2f2f2",
+  frameBorderColor: "#e1e7e9",
+};
 
 const colorsDark = {
   mainTextColor: "#ffffff",
@@ -54,8 +55,8 @@ const colorsDark = {
   outletBackgroundColor: "#171820",
   sidebarBackgroundColor: "#13151a",
   accentBackgroundColor: "#3e85f3",
-  modalBorder:'#DCE3E5CC',
-
+  modalBorder: "#DCE3E5CC",
+  iconColor: "#FFFFFF",
   /* tasks */
   taskHighColor: "#ea3d65",
   taskMedColor: "#f3b249",
@@ -75,4 +76,4 @@ const colorsDark = {
   frameBorderColor: "#42434c",
 };
 
-export { colorsLight ,colorsDark}
+export { colorsLight, colorsDark };

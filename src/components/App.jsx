569748749c15169bd0fd04fb5af17b32b5bf5d6e
Halai.Dasha <dashka04@gmail.com>
2023-04-22 17:32:13 +0300

    feat(kit):create avatar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 40,
-        color: "#010101",
-      }}
-    >
-      goosetrack-team-project-front
-    </div>
-  );
+    return (
+        <div
+            style={{
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 40,
+                color: '#010101',
+            }}
+        >
+            goosetrack-team-project-front
+        </div>
+    );
 };
---
+++ src/core/kit/Avatar.jsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import { Icon } from './Icon';
+import { iconNames } from 'assets/icons/iconNames';
+
+export const Avatar = ({ size, plusIcon }) => {
+    const [value, setValue] = useState('');
+
+    const handleChange = e => {
+        const file = e.target.files[0];
+        let blob = new Blob([file], { type: 'image/jpeg' });
+        const objURL = URL.createObjectURL(blob);
+        setValue(objURL);
+    };
+    return (
+        <AvatarContainer size={size}>
+            <AvatarInput id="file" type="file" onChange={handleChange} />
+            <AvatarLabel htmlFor="file">
+                {!value ? (
+                    <UserIconWrapper>
+                        <Icon name={iconNames.user} size="48px" />
+                    </UserIconWrapper>
+                ) : (
+                    <AvatarImage src={value} alt="user avatar" />
+                )}
+            </AvatarLabel>
+            {plusIcon && (
+                <PlusIconWrapper>
+                    <Icon name={iconNames.plus} size="18px" />
+                </PlusIconWrapper>
+            )}
+        </AvatarContainer>
+    );
+};
+
+Avatar.propTypes = {
+    size: PropTypes.string.isRequired,
+    plusIcon: PropTypes.string,
+};
+
+const AvatarContainer = styled.div(({ size }) => ({
+    position: 'relative',
+
+    width: size,
+    height: size,
+
+    borderRadius: '50%',
+    border: '2px solid #3E85F3',
+}));
+
+const AvatarInput = styled.input({
+    visibility: 'hidden',
+});
+const AvatarLabel = styled.label({
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+
+    width: 'inherit',
+    height: 'inherit',
+    borderRadius: '50%',
+});
+const UserIconWrapper = styled.div({
+    width: 'inherit',
+    height: 'inherit',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    color: '#3E85F3',
+});
+const AvatarImage = styled.img({
+    width: 'inherit',
+    height: 'inherit',
+    borderRadius: '50%',
+
+    objectFit: 'cover',
+});
+const PlusIconWrapper = styled.div({
+    position: 'absolute',
+    bottom: '0',
+    right: '10%',
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    width: '24px',
+    height: '24px',
+    borderRadius: '50%',
+
+    color: 'white',
+    backgroundColor: '#3E85F3',
+});

2cf32c77ec91efedc68de5f7161b55699244a7c8
alionachykal <106018567+alionachykal@users.noreply.github.com>
2023-04-23 17:18:08 +0300

    Translation and trn btn
     made translation btn

--- src/components/RegisterForm/index.jsx
+++ src/components/RegisterForm/index.jsx
@@ -1,79 +1,115 @@
 import { validationSchema } from 'schemas/registerFormValidation';
+import { useTranslation } from 'react-i18next';
 
 import { Input } from 'core/kit/Input';
 import { Button } from 'core/kit/Button';
 
 import { Formik } from 'formik';
 
+const LangaguesBar = () => {
+    const LANGAGUES = ['en', 'ua'];
+    const { i18n } = useTranslation();
+
+    return (
+        <div>
+            <ul>
+                {LANGAGUES.map(langague => (
+                    <li key={langague}>
+                        <button
+                            id={
+                                i18n.resolvedLanguage === langague
+                                    ? 'selectedLang'
+                                    : 'langId'
+                            }
+                            onClick={() => i18n.changeLanguage(langague)}
+                        >
+                            {langague}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
 export const RegisterForm = () => {
+    const { t } = useTranslation();
+
     const onSubmit = values => {
         console.log(values);
     };
 
     return (
-        <Formik
-            initialValues={{
-                email: '',
-                name: '',
-                password: '',
-            }}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-        >
-            {({
-                values,
-                errors,
-                handleSubmit,
-                handleChange,
-                isValid,
-                handleBlur,
-                touched,
-            }) => (
-                <form onSubmit={handleSubmit} autoComplete="off">
-                    <Input
-                        name="email"
-                        type="text"
-                        labelTitle="Email"
-                        placeholder="Enter your email"
-                        handleBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.email}
-                        error={
-                            touched.email && errors.email ? errors.email : ''
-                        }
-                    />
-                    <Input
-                        name="name"
-                        type="text"
-                        labelTitle="Name"
-                        placeholder="Enter your name"
-                        handleBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.name}
-                        error={touched.name && errors.name ? errors.name : ''}
-                    />
+        <>
+            <LangaguesBar />
+            <Formik
+                initialValues={{
+                    email: '',
+                    name: '',
+                    password: '',
+                }}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {({
+                    values,
+                    errors,
+                    handleSubmit,
+                    handleChange,
+                    isValid,
+                    handleBlur,
+                    touched,
+                }) => (
+                    <form onSubmit={handleSubmit} autoComplete="off">
+                        <Input
+                            name="email"
+                            type="text"
+                            labelTitle={t('labelTitleEmail')}
+                            placeholder={t('inputPlaceholderEmail')}
+                            handleBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.email}
+                            error={
+                                touched.email && errors.email
+                                    ? errors.email
+                                    : ''
+                            }
+                        />
+                        <Input
+                            name="name"
+                            type="text"
+                            labelTitle={t('labelTitleName')}
+                            placeholder={t('inputPlaceholderName')}
+                            handleBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.name}
+                            error={
+                                touched.name && errors.name ? errors.name : ''
+                            }
+                        />
 
-                    <Input
-                        name="password"
-                        type="password"
-                        labelTitle="Password"
-                        placeholder="Enter password"
-                        handleBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values.password}
-                        error={
-                            touched.password && errors.password
-                                ? errors.password
-                                : ''
-                        }
-                    />
-                    <Button
-                        type="submit"
-                        disabled={!isValid}
-                        title={!isValid ? 'disabled' : 'Submit'}
-                    />
-                </form>
-            )}
-        </Formik>
+                        <Input
+                            name="password"
+                            type="password"
+                            labelTitle={t('labelTitlePassword')}
+                            placeholder={t('inputPlaceholderPassword')}
+                            handleBlur={handleBlur}
+                            onChange={handleChange}
+                            value={values.password}
+                            error={
+                                touched.password && errors.password
+                                    ? errors.password
+                                    : ''
+                            }
+                        />
+                        <Button
+                            type="submit"
+                            disabled={!isValid}
+                            title={!isValid ? t('button') : t('btnSubmit')}
+                        />
+                    </form>
+                )}
+            </Formik>
+        </>
     );
 };
--- src/core/i18n/i18n.js
+++ src/core/i18n/i18n.js
@@ -1,9 +1,36 @@
-import i18next from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
 // to do translaion.json
-const FRONTEND_BASE_URL = "http://localhost:3000";
+const FRONTEND_BASE_URL = 'http://localhost:3000';
+
+const resources = {
+    en: {
+        translation: {
+            button: 'disabled',
+            btnSubmit: 'Submit',
+            labelTitleName: 'Name',
+            labelTitleEmail: 'Email',
+            labelTitlePassword: 'Password',
+            inputPlaceholderEmail: 'Enter your email',
+            inputPlaceholderName: 'Enter your name',
+            inputPlaceholderPassword: 'Enter your password',
+        },
+    },
+    ua: {
+        translation: {
+            button: 'Вимкнена',
+            btnSubmit: 'Зареєструватися',
+            labelTitleName: "Ім'я",
+            labelTitlePassword: 'Пароль',
+            labelTitleEmail: 'Імейл',
+            inputPlaceholderEmail: 'Введіть свою електронну адресу',
+            inputPlaceholderName: "Введіть своє ім'я ",
+            inputPlaceholderPassword: 'Введіть свій пароль',
+        },
+    },
+};
 
 i18next
     .use(initReactI18next)
@@ -13,9 +40,10 @@ i18next
         backend: {
             loadPath: `${FRONTEND_BASE_URL}/goosetrack-team-project-front/locales/{{lng}}/translation.json`,
         },
+        resources,
         fallbackLng: 'en',
         whitelist: ['en', 'uk'],
-        debug: false,
+        debug: true,
 
         detection: {
             order: ['localStorage', 'cookie'],

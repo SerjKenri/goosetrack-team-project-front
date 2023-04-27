86018da1f440761f3a09120a312a044283aaf9ba
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-04-27 13:47:59 +0300

    fix

--- src/components/LogoutBtn/LogOutButton.jsx
+++ src/components/LogoutBtn/LogOutButton.jsx
@@ -1,33 +1,33 @@
-import { iconNames } from "assets/icons/iconNames";
-import { useMatchMedia } from "core/hooks/use-match-media";
-import { Button } from "core/kit/Button"
-import { ButtonDifference } from "core/kit/Button";
-import { useDispatch, useSelector } from "react-redux";
-import { selectTokenState } from "redux/auth/auth.selectors";
-import { logoutUser } from "redux/operations";
-
+import { iconNames } from 'assets/icons/iconNames';
+import { useMatchMedia } from 'core/hooks/useMatchMedia';
+import { Button } from 'core/kit/Button';
+import { ButtonDifference } from 'core/kit/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectTokenState } from 'redux/auth/auth.selectors';
+import { logoutUser } from 'redux/operations';
 
 export const LogOutButton = () => {
+    const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-    const token = useSelector(selectTokenState)
+    const token = useSelector(selectTokenState);
 
     const handleLogout = () => {
-        dispatch(logoutUser())
-    }
+        dispatch(logoutUser());
+    };
 
-    const { isMobile } = useMatchMedia()
-    
-    const size = isMobile ? '18px' : '20px'
+    const { isMobile } = useMatchMedia();
 
-    return <Button 
-        type="button"
-        differentStyles={ButtonDifference.primary}
-        title="Log out"
-        onClick={handleLogout}
-        iconName={iconNames.logout}
-        iconSize={size}
-        disabled = {token ? false : true}
-    />
-}
+    const size = isMobile ? '18px' : '20px';
+
+    return (
+        <Button
+            type="button"
+            differentStyles={ButtonDifference.primary}
+            title="Log out"
+            onClick={handleLogout}
+            iconName={iconNames.logout}
+            iconSize={size}
+            disabled={token ? false : true}
+        />
+    );
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import { ManageThemeProvider } from 'core/theme/ThemeContext';

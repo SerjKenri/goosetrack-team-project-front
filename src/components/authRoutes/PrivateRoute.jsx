40e7282969cc53d43d5a14e454df09aa7e34ac33
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-04-28 13:14:12 +0300

    fix and turn on Private Public roures

--- src/components/authRoutes/PrivateRoute.jsx
+++ src/components/authRoutes/PrivateRoute.jsx
@@ -1,14 +1,10 @@
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import { selectAuthToken } from 'redux/auth/auth.selectors';
-// import { ROUTING } from 'core/utils/constantsRouting';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { selectTokenState } from 'redux/auth/auth.selectors';
+import { ROUTING } from 'core/utils/constantsRouting';
 
 const PrivateRoute = () => {
-    // const token = useSelector(selectAuthToken);
-    // return token ? (
-    //     <Navigate to={ROUTING.ACCOUNT} />
-    // ) : (
-    //     <Navigate to={ROUTING.LOGIN} replace />
-    // );
+    const token = useSelector(selectTokenState);
+    return token ? <Outlet /> : <Navigate to={ROUTING.LOGIN} replace />;
 };
 export default PrivateRoute;
--- src/components/authRoutes/PublicRoute.jsx
+++ src/components/authRoutes/PublicRoute.jsx
@@ -1,15 +1,11 @@
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import { selectAuthToken } from 'redux/auth/auth.selectors';
-// import { ROUTING } from 'core/utils/constantsRouting';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { selectTokenState } from 'redux/auth/auth.selectors';
+import { ROUTING } from 'core/utils/constantsRouting';
 
 const PublicRoute = () => {
-    // const token = useSelector(selectAuthToken);
-    // const token = '';
-    // return token ? (
-    //     <Navigate to={ROUTING.ACCOUNT} replace />
-    // ) : (
-    //     <Navigate to={ROUTING.LOGIN} />
-    // );
+    const token = useSelector(selectTokenState);
+    // const token = '432234fsadfdsf';
+    return token ? <Navigate to={ROUTING.ACCOUNT} replace /> : <Outlet />;
 };
 export default PublicRoute;

51ae528bcc73bbb0c57655e77494adb1fb7e26a3
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-04-28 10:34:02 +0300

    add changes

--- src/redux/tasks/tasks.slice.js
+++ src/redux/tasks/tasks.slice.js
@@ -11,44 +11,42 @@ const handleRejected = (state, action) => {
     state.tasks.error = action.payload;
 };
 
+const handleFulfilled = (state, action) => {
+    state.tasks.isLoading = false;
+    state.tasks.error = null;
+    state.tasks.items = action.payload;
+};
+
 const taskSlice = createSlice({
     name: 'tasks',
     initialState: tasksInitState,
-    // reducers: {},
+    reducers: {},
 
     extraReducers: builder => {
         builder
             .addCase(fetchTasks.pending, handlePending)
             .addCase(fetchTasks.rejected, handleRejected)
-            .addCase(fetchTasks.fulfilled, (state, action) => {
-                state.tasks.isLoading = false;
-                state.tasks.error = null;
-                state.tasks.items = action.payload;
-            });
-        builder
+            .addCase(fetchTasks.fulfilled, handleFulfilled)
             .addCase(addTask.pending, handlePending)
             .addCase(addTask.rejected, handleRejected)
-            .addCase(addTask.fulfilled, (state, action) => {
-                state.tasks.isLoading = false;
-                state.tasks.error = null;
-                state.tasks.items.push(action.payload);
-            });
-        builder
+            .addCase(addTask.fulfilled, (state, { payload }) => {
+                state.tasks.items.push(payload);
+            })
             .addCase(updateTask.pending, handlePending)
             .addCase(updateTask.rejected, handleRejected)
-            .addCase(updateTask.fulfilled, (state, action) => {
-                state.tasks.isLoading = false;
-                state.tasks.error = null;
-                state.tasks.items = action.payload.tasks.items;
-            });
-        builder
+            .addCase(updateTask.fulfilled, (state, { payload }) => {
+                const index = state.tasks.items.findIndex(
+                    task => task.id === payload.id
+                );
+                if (index !== -1) {
+                    state.tasks.items[index] = payload;
+                }
+            })
             .addCase(delTask.pending, handlePending)
             .addCase(delTask.rejected, handleRejected)
-            .addCase(delTask.fulfilled, (state, action) => {
-                state.tasks.isLoading = false;
-                state.tasks.error = null;
+            .addCase(delTask.fulfilled, (state, { payload }) => {
                 state.tasks.items = state.tasks.items.filter(
-                    task => task.id !== action.payload.id
+                    task => task.id !== payload.id
                 );
             });
     },

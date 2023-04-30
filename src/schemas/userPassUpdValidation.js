17b9ccf709ae185169933b67bed198b14ad7c8d0
Olexii-US <alyosha.us@gmail.com>
2023-04-30 23:54:39 +0300

    add language

--- src/schemas/userPassUpdValidation.js
+++ src/schemas/userPassUpdValidation.js
@@ -1,18 +1,18 @@
 import * as Yup from 'yup';
 
-const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
+// const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 const userUpdPassSchema = Yup.object().shape({
     currentPassword: Yup.string()
         .min(5, 'minLengthPass')
-        .matches(passwordRules, 'errorPassword')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/, 'errorPassword')
         .required('errorRequired'),
     newPassword: Yup.string()
         .min(5, 'minLengthPass')
-        .matches(passwordRules, 'errorPassword')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/, 'errorPassword')
         .required('errorRequired'),
     confirmPassword: Yup.string()
         .min(5, 'minLengthPass')
-        .matches(passwordRules, 'errorPassword')
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/, 'errorPassword')
         .required('errorRequired'),
 });
 

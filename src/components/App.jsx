1066ed65093ddc4f0e400bdeee551881cc8c19f5
BOGDAN YANISHEVSKYY <106109430+Bogdan-Yanish@users.noreply.github.com>
2023-05-01 15:10:00 +0300

    add callendar toolbar (there is no logic)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,5 +47,6 @@ export const App = () => {
                 </Route>
             </Routes>
         </Suspense>
+        
     );
 };
--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,2 +1,13 @@
-const CalendarPage = () => {};
+import { CalendarToolbar } from "pages/CalendarToolbar/CalendarToolbar";
+import { CalendarTable } from "components/CalendarTable/CalendarTable";
+
+const CalendarPage = () => {
+    return (
+        <>
+            <CalendarToolbar/>
+            <CalendarTable/>
+        </>
+    );
+};
+
 export default CalendarPage;

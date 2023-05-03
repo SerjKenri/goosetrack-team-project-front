0a3b40479b2e0c0ab379241575ce067115eb11bf
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-05-03 15:17:27 +0300

    add pagination

--- src/pages/CalendarToolbar/CalendarToolbar.jsx
+++ src/pages/CalendarToolbar/CalendarToolbar.jsx
@@ -1,35 +1,11 @@
 import styled from 'styled-components';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
-
 import { PeriodPaginator } from 'components/PeriodPaginator/PeriodPaginator';
 import { PeriodTypeSelect } from 'components/PeriodTypeSelect/PeriodTypeSelect';
-import { usePeriodModule } from 'core/hooks/usePeriodModule';
-import { dateFormat } from 'core/hooks/usePeriodTitle';
 
 export const CalendarToolbar = ({ handlePrevPeriod, handleNextPeriod }) => {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const periodType = usePeriodModule();
-    const date = new Date();
-    const startPeiodType = dateFormat.month;
-
-    const { currentDate } = useParams();
-
-    const setDate = date => {
-        const pathnameArr = location.pathname.split('/');
-        pathnameArr[pathnameArr.length - 1] = date;
-
-        navigate({ pathname: pathnameArr.join('/') });
-    };
-
     return (
         <ToolbarWrap>
-            <PeriodPaginator
-                periodType={periodType ? periodType : startPeiodType}
-                date={currentDate !== ':currentDate' ? currentDate : date}
-                setDate={setDate}
-            />
+            <PeriodPaginator />
             <PeriodTypeSelect />
         </ToolbarWrap>
     );
@@ -39,10 +15,10 @@ export const ToolbarWrap = styled.div`
     display: flex;
     flex-direction: column;
     border-radius: 8px;
-    margin-bottom: 14px;
     align-items: flex-start;
     padding-left: 20px;
     gap: 18px;
+    width: 600px;
 
     @media screen and (min-width: 768px) {
         flex-direction: row;
